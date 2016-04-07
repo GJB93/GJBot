45b8134262c635e271b49da61c37dc2e2a5a2832
gjb93 <grahambyrne93@gmail.com>
2016-04-07 13:24:36 +0100

    Added some parrotting messages to the bot

--- src/ie/dit/Main.java
+++ src/ie/dit/Main.java
@@ -1,5 +1,10 @@
 package ie.dit;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Created by Graham on 06-Apr-16.
  */
@@ -8,6 +13,7 @@ public class Main {
     public static void main(String[] args)
     {
         TwitchClient client;
+        Scanner inputReader = new Scanner(System.in);
 
         client = new TwitchClient(args[0], args[1]);
 
