c97d7de86e53e176cbfaf37f98697a53789521a4
GJB93 <grahambyrne93@gmail.com>
2016-04-12 14:53:29 +0100

    removing workspace.xml

--- src/ie/dit/Main.java
+++ src/ie/dit/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args)
     {
         TwitchClient client;
-        APILibrary api = new APILibrary(args[2]);
 
         client = new TwitchClient(args[0], args[1], args[2]);
 
